8f642e6f1fb5cce5be3a3b4778fb87d18459a23c
Rakuyo <rakuyo.mo@gmail.com>
2020-09-09 17:27:05 +0800

    Add JSONPreview

---
+++ JSONPreview/JSONPreview/Core/JSONPreview.swift
@@ -0,0 +1,14 @@
+//
+//  JSONPreview.swift
+//  JSONPreview
+//
+//  Created by Rakuyo on 2020/9/9.
+//  Copyright © 2020 Rakuyo. All rights reserved.
+//
+
+import UIKit
+
+open class JSONPreview: UIView {
+    
+    
+}
