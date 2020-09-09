46e8bc6e537b756e69f0000b775a2eb57a5e7129
Rakuyo <rakuyo.mo@gmail.com>
2020-09-09 17:25:48 +0800

    Initialize the project

---
+++ JSONPreview/JSONPreview/Other/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  JSONPreview
+//
+//  Created by Rakuyo on 2020/9/9.
+//  Copyright © 2020 Rakuyo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
