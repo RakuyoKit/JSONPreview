7bdcf26819cfb9dd8233b9e2f74d369d287faa0f
Rakuyo <rakuyo.mo@gmail.com>
2020-09-10 10:45:09 +0800

    Add the implementation of JSONSlice

--- JSONPreview/JSONPreview/Core/JSONSlice.swift
+++ JSONPreview/JSONPreview/Core/JSONSlice.swift
@@ -7,3 +7,41 @@
 //
 
 import Foundation
+
+/// Used to represent a certain part of JSON
+public struct JSONSlice {
+    
+    /// Initialization method
+    ///
+    /// - Parameters:
+    ///   - level: Indentation level
+    ///   - expand: The complete content of the JSON slice in the expanded state
+    ///   - folded: The summary content of the JSON slice in the folded state
+    public init(
+        level: Int,
+        expand: (String, [NSAttributedString.Key : Any]),
+        folded: (String, [NSAttributedString.Key : Any])? = nil
+    ) {
+        
+        self.isFolded = false
+        self.level = level
+        
+        self.expand = NSAttributedString(string: expand.0, attributes: expand.1)
+        
+        if let folded = folded {
+            self.folded = NSAttributedString(string: folded.0, attributes: folded.1)
+        }
+    }
+    
+    /// Whether it is currently folded
+    public var isFolded: Bool
+    
+    /// Indentation level
+    public var level: Int
+    
+    /// The complete content of the JSON slice in the expanded state
+    public var expand: NSAttributedString
+    
+    /// The summary content of the JSON slice in the folded state
+    public var folded: NSAttributedString?
+}
