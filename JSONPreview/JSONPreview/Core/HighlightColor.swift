1bd59dd89e584e6f9178df0d9f88c3daa115de71
Rakuyo <rakuyo.mo@gmail.com>
2020-09-10 13:07:30 +0800

    Improve the initialization method of HighlightColor

--- JSONPreview/JSONPreview/Core/HighlightColor.swift
+++ JSONPreview/JSONPreview/Core/HighlightColor.swift
@@ -11,6 +11,26 @@ import UIKit
 /// Color configuration for highlight
 public struct HighlightColor {
     
+    public init(
+        keyWord: UIColor,
+        key: UIColor,
+        link: UIColor,
+        string: UIColor,
+        number: UIColor,
+        jsonBackground: UIColor,
+        lineBackground: UIColor,
+        lineText: UIColor
+    ) {
+        self.keyWord = keyWord
+        self.key = key
+        self.link = link
+        self.string = string
+        self.number = number
+        self.jsonBackground = jsonBackground
+        self.lineBackground = lineBackground
+        self.lineText = lineText
+    }
+    
     /// Keyword color. Including `{}`, `[]`, `:`, `,`
     public let keyWord: UIColor
     
@@ -34,4 +54,69 @@ public struct HighlightColor {
     
     /// Text color in line number area
     public let lineText: UIColor
+}
+
+public extension HighlightColor {
+    
+    typealias ColorTuple = (red: CGFloat, green: CGFloat, blue: CGFloat, alpha: CGFloat)
+    
+    init(
+        keyWord: ColorTuple,
+        key: ColorTuple,
+        link: ColorTuple,
+        string: ColorTuple,
+        number: ColorTuple,
+        jsonBackground: ColorTuple,
+        lineBackground: ColorTuple,
+        lineText: ColorTuple
+    ) {
+        
+        self.keyWord = UIColor(keyWord)
+        self.key = UIColor(key)
+        self.link = UIColor(link)
+        self.string = UIColor(string)
+        self.number = UIColor(number)
+        self.jsonBackground = UIColor(jsonBackground)
+        self.lineBackground = UIColor(lineBackground)
+        self.lineText = UIColor(lineText)
+    }
+    
+    typealias ColorHexTuple = (hex: Int, alpha: CGFloat)
+    
+    init(
+        keyWord: ColorHexTuple,
+        key: ColorHexTuple,
+        link: ColorHexTuple,
+        string: ColorHexTuple,
+        number: ColorHexTuple,
+        jsonBackground: ColorHexTuple,
+        lineBackground: ColorHexTuple,
+        lineText: ColorHexTuple
+    ) {
+        
+        self.keyWord = UIColor(hex: keyWord.hex, alpha: keyWord.alpha)
+        self.key = UIColor(hex: key.hex, alpha: key.alpha)
+        self.link = UIColor(hex: link.hex, alpha: link.alpha)
+        self.string = UIColor(hex: string.hex, alpha: string.alpha)
+        self.number = UIColor(hex: number.hex, alpha: number.alpha)
+        self.jsonBackground = UIColor(hex: jsonBackground.hex, alpha: jsonBackground.alpha)
+        self.lineBackground = UIColor(hex: lineBackground.hex, alpha: lineBackground.alpha)
+        self.lineText = UIColor(hex: lineText.hex, alpha: lineText.alpha)
+    }
+}
+
+fileprivate extension UIColor {
+    
+    convenience init(_ colorTuple: HighlightColor.ColorTuple) {
+        self.init(red: colorTuple.red, green: colorTuple.green, blue: colorTuple.blue, alpha: colorTuple.alpha)
+    }
+    
+    convenience init(hex: Int, alpha: CGFloat = 1) {
+        
+        let red   = CGFloat((hex & 0xFF0000) >> 16) / 255.0
+        let green = CGFloat((hex & 0xFF00) >> 8) / 255.0
+        let blue  = CGFloat(hex & 0xFF) / 255.0
+        
+        self.init(red: red, green: green, blue: blue, alpha: alpha)
+    }
 }
