757ef09b2b84ebb8701a63f720642953699573b6
Rakuyo <rakuyo.mo@gmail.com>
2020-09-11 14:28:38 +0800

    Add some test

--- JSONPreview/JSONPreview/Other/ViewController.swift
+++ JSONPreview/JSONPreview/Other/ViewController.swift
@@ -12,9 +12,26 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let json = """
+        {
+            "key" : {
+                "value" : [
+                    null,
+                    "test",
+                    true,
+                    false
+                ]
+            },
+            "key2" : [],
+            "key3" : {},
+            "key4" : 3.1415926,
+            "key5" : -50,
+            "key6" : -50.0,
+            "key7" : 50
+        }
+        """
+        
+        print(JSONDecorator.highlight(json).map { $0.expand.string })
     }
-
-
 }
-
