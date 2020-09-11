c5c338fe7e19c6d13d431c480ef4ca5dc3819400
Rakuyo <rakuyo.mo@gmail.com>
2020-09-11 17:20:29 +0800

    Fix some problems in handling json token
    Modified the algorithm implementation method, from recursion to loop

--- JSONPreview/JSONPreview/Core/JSONLexer.swift
+++ JSONPreview/JSONPreview/Core/JSONLexer.swift
@@ -93,7 +93,7 @@ public extension JSONLexer {
         }
         
         // Parse the remaining content
-        lexer.findNextToken(in: _json)
+        lexer.getremainingTokens(in: _json)
         
         return lexer.tokens
     }
@@ -101,199 +101,203 @@ public extension JSONLexer {
 
 private extension JSONLexer {
     
-    /// Find the next token.
+    /// Get the remaining token.
     ///
     /// - Parameter json: Target json.
-    func findNextToken(in json: String) {
+    func getremainingTokens(in json: String) {
         
         guard !json.isEmpty else { return }
         
         var tmpJSON = json
         
-        let last = tokens.last
-        
-        switch tmpJSON.removeFirst() {
+        // Due to some restrictions, it cannot be implemented with recursion,
+        // so it can only use the loop algorithm
+        for _ in  0 ..< json.count {
             
-            // Filter spaces and newlines.
-            // This type of character will not cause format errors.
-        case " ", "\t", "\n": break
+            guard !tmpJSON.isEmpty else { break }
             
-        case ":":
+            let last = tokens.last
             
-            switch last {
-            
-            case .objectKey:
-                tokens.append(.colon)
+            switch tmpJSON.removeFirst() {
                 
-            default:
-                tokens.append(.unknown(json))
-                return
-            }
-            
-        case ",":
-            
-            switch last {
-            
-            case .null, .string, .number, .boolean, .objectEnd, .arrayEnd:
-                tokens.append(.comma)
+                // Filter spaces and newlines.
+                // This type of character will not cause format errors.
+            case " ", "\t", "\n": break
                 
-            default:
-                tokens.append(.unknown(json))
-                return
-            }
-            
-        case "{":
-            
-            if last == nil || last! != .objectBegin {
-                beginNodes.append(.object)
-                tokens.append(.objectBegin)
+            case ":":
                 
-            } else {
-                tokens.append(.unknown(json))
-                return
-            }
-            
-        case "}":
-            
-            if beginNodes.last == .object {
-                beginNodes.removeLast()
-                tokens.append(.objectEnd)
+                switch last {
                 
-            } else {
-                tokens.append(.unknown(json))
-                return
-            }
-            
-        case "[":
-            
-            if last == nil || last! != .arrayBegin {
-                beginNodes.append(.array)
-                tokens.append(.arrayBegin)
+                case .objectKey:
+                    tokens.append(.colon)
+                    
+                default:
+                    tokens.append(.unknown(json))
+                    return
+                }
                 
-            } else {
-                tokens.append(.unknown(json))
-                return
-            }
-            
-        case "]":
-            
-            if beginNodes.last == .array {
-                beginNodes.removeLast()
-                tokens.append(.arrayEnd)
+            case ",":
                 
-            } else {
-                tokens.append(.unknown(json))
-                return
-            }
-            
-        case "\"":
-            
-            // Determine whether the string is a complete string node.
-            guard let index = tmpJSON.firstIndex(of: "\"") else {
-                tokens.append(.unknown(json))
-                return
-            }
-            
-            let startIndex = tmpJSON.startIndex
-            
-            let string = String(tmpJSON[startIndex ..< index])
-            
-            switch last {
+                switch last {
                 
-            case .objectBegin:
-                tokens.append(.objectKey(string))
+                case .null, .string, .number, .boolean, .objectEnd, .arrayEnd:
+                    tokens.append(.comma)
+                    
+                default:
+                    tokens.append(.unknown(json))
+                    return
+                }
                 
-            case .arrayBegin, .colon:
-                tokens.append(.string(string))
+            case "{":
                 
-            case .comma:
-                
-                // Need to determine whether it is currently in the object or in the array.
-                tokens.append(beginNodes.last == .object ? .objectKey(string) : .string(string))
-                
-            default:
-                tokens.append(.unknown(json))
-                return
-            }
-            
-            tmpJSON.removeSubrange(startIndex ... index)
-            
-        case "n":
-            
-            let startIndex = tmpJSON.startIndex
-            let bounds = startIndex ..< tmpJSON.index(startIndex, offsetBy: 3)
-            
-            // The character `n` should be processed in `case "\""`.
-            // If it is not `null` and it hits the `case`, it should be classified as a syntax error.
-            guard tmpJSON[bounds] == "ull" else {
-                tokens.append(.unknown(json))
-                return
-            }
-            
-            tokens.append(.null)
-            tmpJSON.removeSubrange(bounds)
-            
-        case "t":
-            
-            let startIndex = tmpJSON.startIndex
-            let bounds = startIndex ..< tmpJSON.index(startIndex, offsetBy: 3)
-            
-            // The character `t` should be processed in `case "\""`.
-            // If it is not `true` and it hits the `case`, it should be classified as a syntax error.
-            guard tmpJSON[bounds] == "rue" else {
-                tokens.append(.unknown(json))
-                return
-            }
-            
-            tokens.append(.boolean(true))
-            tmpJSON.removeSubrange(bounds)
-            
-        case "f":
-            
-            let startIndex = tmpJSON.startIndex
-            let bounds = startIndex ..< tmpJSON.index(startIndex, offsetBy: 4)
-            
-            // The character `f` should be processed in `case "\""`.
-            // If it is not `false` and it hits the `case`, it should be classified as a syntax error.
-            guard tmpJSON[bounds] == "alse" else {
-                tokens.append(.unknown(json))
-                return
-            }
-            
-            tokens.append(.boolean(false))
-            tmpJSON.removeSubrange(bounds)
-            
-        case let value where !tmpJSON.isEmpty && (value == "-" || value.isNumber):
-            
-            var number = String(value)
-            var _first: Character? = tmpJSON.removeFirst()
-            
-            while let first = _first {
-                
-                if first.isNumber || first == "." {
-                    number += String(first)
-                    _first = tmpJSON.removeFirst()
+                if last == nil || last! != .objectBegin {
+                    beginNodes.append(.object)
+                    tokens.append(.objectBegin)
                     
                 } else {
-                    
-                    tmpJSON = String(first) + tmpJSON
-                    _first = nil
+                    tokens.append(.unknown(json))
+                    return
                 }
-            }
-            
-            guard !number.hasSuffix(".") else {
+                
+            case "}":
+                
+                if beginNodes.last == .object {
+                    beginNodes.removeLast()
+                    tokens.append(.objectEnd)
+                    
+                } else {
+                    tokens.append(.unknown(json))
+                    return
+                }
+                
+            case "[":
+                
+                if last == nil || last! != .arrayBegin {
+                    beginNodes.append(.array)
+                    tokens.append(.arrayBegin)
+                    
+                } else {
+                    tokens.append(.unknown(json))
+                    return
+                }
+                
+            case "]":
+                
+                if beginNodes.last == .array {
+                    beginNodes.removeLast()
+                    tokens.append(.arrayEnd)
+                    
+                } else {
+                    tokens.append(.unknown(json))
+                    return
+                }
+                
+            case "\"":
+                
+                // Determine whether the string is a complete string node.
+                guard let index = tmpJSON.firstIndex(of: "\"") else {
+                    tokens.append(.unknown(json))
+                    return
+                }
+                
+                let startIndex = tmpJSON.startIndex
+                
+                let string = String(tmpJSON[startIndex ..< index])
+                
+                switch last {
+                    
+                case .objectBegin:
+                    tokens.append(.objectKey(string))
+                    
+                case .arrayBegin, .colon:
+                    tokens.append(.string(string))
+                    
+                case .comma:
+                    
+                    // Need to determine whether it is currently in the object or in the array.
+                    tokens.append(beginNodes.last == .object ? .objectKey(string) : .string(string))
+                    
+                default:
+                    tokens.append(.unknown(json))
+                    return
+                }
+                
+                tmpJSON.removeSubrange(startIndex ... index)
+                
+            case "n":
+                
+                let startIndex = tmpJSON.startIndex
+                let bounds = startIndex ..< tmpJSON.index(startIndex, offsetBy: 3)
+                
+                // The character `n` should be processed in `case "\""`.
+                // If it is not `null` and it hits the `case`, it should be classified as a syntax error.
+                guard tmpJSON[bounds] == "ull" else {
+                    tokens.append(.unknown(json))
+                    return
+                }
+                
+                tokens.append(.null)
+                tmpJSON.removeSubrange(bounds)
+                
+            case "t":
+                
+                let startIndex = tmpJSON.startIndex
+                let bounds = startIndex ..< tmpJSON.index(startIndex, offsetBy: 3)
+                
+                // The character `t` should be processed in `case "\""`.
+                // If it is not `true` and it hits the `case`, it should be classified as a syntax error.
+                guard tmpJSON[bounds] == "rue" else {
+                    tokens.append(.unknown(json))
+                    return
+                }
+                
+                tokens.append(.boolean(true))
+                tmpJSON.removeSubrange(bounds)
+                
+            case "f":
+                
+                let startIndex = tmpJSON.startIndex
+                let bounds = startIndex ..< tmpJSON.index(startIndex, offsetBy: 4)
+                
+                // The character `f` should be processed in `case "\""`.
+                // If it is not `false` and it hits the `case`, it should be classified as a syntax error.
+                guard tmpJSON[bounds] == "alse" else {
+                    tokens.append(.unknown(json))
+                    return
+                }
+                
+                tokens.append(.boolean(false))
+                tmpJSON.removeSubrange(bounds)
+                
+            case let value where !tmpJSON.isEmpty && (value == "-" || value.isNumber):
+                
+                var number = String(value)
+                var _first: Character? = tmpJSON.removeFirst()
+                
+                while let first = _first {
+                    
+                    if first.isNumber || first == "." {
+                        number += String(first)
+                        _first = tmpJSON.removeFirst()
+                        
+                    } else {
+                        
+                        tmpJSON = String(first) + tmpJSON
+                        _first = nil
+                    }
+                }
+                
+                guard !number.hasSuffix(".") else {
+                    tokens.append(.unknown(json))
+                    return
+                }
+                
+                tokens.append(.number(number))
+                
+            default:
                 tokens.append(.unknown(json))
                 return
             }
-            
-            tokens.append(.number(number))
-            
-        default:
-            tokens.append(.unknown(json))
-            return
         }
-        
-        // Recursively process the remaining content
-        findNextToken(in: tmpJSON)
     }
 }
