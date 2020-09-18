18d75af7324e295d3131760d806aa214a1c2dd6c
Rakuyo <rakuyo.mo@gmail.com>
2020-09-18 15:35:30 +0800

    update test json

--- JSONPreview/JSONPreview/Other/ViewController.swift
+++ JSONPreview/JSONPreview/Other/ViewController.swift
@@ -43,71 +43,41 @@ class ViewController: UIViewController {
         NSLayoutConstraint.activate(constraints)
         
         let json = """
-        {
-            "key_1" : "string",
-            "key_2" : 3.1415926,
-            "key_3" : -50,
-            "key_4" : [],
-            "key_5" : {},
-            "key_6" : {
-                "key_6_1" : null,
-                "key_6_2" : [
-                    "value_1",
-                    "value_2"
-                ],
-                "key_6_3" : {
-                    "bool_1" : true,
-                    "bool_2" : false,
-                    "empty_string" : ""
-                }
-            }
-        },
         [
-            "array_1",
-            3.1415926,
-            -50,
-            true,
-            false,
-            null,
-            {},
             {
-                "some_key" : "some_value"
-            }
-        ],
-        {
-            "key_1" : "string",
-            "key_2" : 3.1415926,
-            "key_3" : -50,
-            "key_4" : [],
-            "key_5" : {},
-            "key_6" : {
-                "key_6_1" : null,
-                "key_6_2" : [
-                    "value_1",
-                    "value_2"
-                ],
-                "key_6_3" : {
-                    "bool_1" : true,
-                    "bool_2" : false,
-                    "empty_string" : ""
+                "key_1" : "string",
+                "key_2" : 3.1415926,
+                "key_3" : -50,
+                "key_4" : [],
+                "key_5" : {},
+                "key_6" : {
+                    "key_6_1" : null,
+                    "key_6_2" : [
+                        "array_1",
+                        3.1415926,
+                        -50,
+                        true,
+                        false,
+                        null,
+                        {},
+                        {
+                            "some_key" : "some_value"
+                        }
+                    ],
+                    "key_6_3" : {
+                        "bool_1" : true,
+                        "bool_2" : false,
+                        "empty_string" : ""
+                    }
                 }
-            }
-        },
-        [
-            "array_1",
-            3.1415926,
-            -50,
-            true,
-            false,
-            null,
-            {},
+            },
             {
-                "some_key" : "A very very very very very very very very very very very very very very very long string"
+                "some_key" : "A very very very very very very very very very very long string."
+            },
+            {
+                {123456}
             }
-        ],
-        {
-            {123456}
-        }
+        ]
         """
         
         previewView.preview(json, style: .default)
