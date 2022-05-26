7e2d94c68365b7a3319ec12c48d4c60c9c28393e
Rakuyo <rakuyo.mo@gmail.com>
2022-05-26 11:30:09 +0800

    Known issue: Apple's format implementation does not support returning incorrectly formatted json
    Changes need to be made to the error (JSONError.unexpectedCharacter) handling in JSONParser.swift.
    
    Prioritize the parsing of correctly formatted json.

--- JSONPreview/Other/ViewController.swift
+++ JSONPreview/Other/ViewController.swift
@@ -110,11 +110,12 @@ class ViewController: UIViewController {
                     }
                 }
             },
-            {
-                {123456}
-            }
         ]
         """
+        
+//        {
+//            {123456}
+//        }
         
         let start = Date().timeIntervalSince1970
         print("will display json")
