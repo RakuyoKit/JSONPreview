296b270abe6c6486e9f299abbec427a7a0b97894
Rakuyo <rakuyo.mo@gmail.com>
2024-03-25 19:05:40 +0800

    [Update] use SceneDelegate in demo

---
+++ Demo/JSONPreviewDemo/Core/SceneDelegate.swift
@@ -0,0 +1,46 @@
+//
+//  SceneDelegate.swift
+//  JSONPreviewDemo
+//
+//  Created by Rakuyo on 2024/3/25.
+//  Copyright © 2024 Rakuyo. All rights reserved.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder {
+    var window: UIWindow?
+}
+
+// MARK: - UIWindowSceneDelegate
+
+extension SceneDelegate: UIWindowSceneDelegate {
+    func scene(
+        _ scene: UIScene,
+        willConnectTo session: UISceneSession,
+        options connectionOptions: UIScene.ConnectionOptions
+    ) {
+        guard let windowScene = scene as? UIWindowScene else { return }
+        
+        let window = UIWindow(windowScene: windowScene)
+        
+        let vc = EntranceTableViewController()
+        
+        window.rootViewController = UINavigationController(rootViewController: vc)
+        window.makeKeyAndVisible()
+        
+        self.window = window
+    }
+    
+    func sceneDidEnterBackground(_ scene: UIScene) {
+        
+    }
+    
+    func sceneWillEnterForeground(_ scene: UIScene) {
+        
+    }
+    
+    func sceneDidBecomeActive(_ scene: UIScene) {
+        
+    }
+}
